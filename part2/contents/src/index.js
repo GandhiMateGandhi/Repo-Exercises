b5e93be1fc3c7a67493df3220248bad6fabd5801
Gandhi <surersculptor@gmail.com>
2020-04-07 00:42:29 +0500

    contents step6 is stucked

--- part2/contents/src/index.js
+++ part2/contents/src/index.js
@@ -1,9 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const App = () => {
+  const course = {
+  id: 1,
+  name: 'Half Stack application development',
+  parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+      id: 1
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+      id: 2
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+      id: 3
+    },
+    {
+      name: 'Redux',
+      exercises: 14,
+      id: 4
+    }
+  ]
+}
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+const Course = () => {
 
+const Header = () => <h1>{course.name}</h1>
+
+const Content = () => {
+  return (
+      <Part />
+    )
+  } 
+
+const Part = () => {
+  return (
+    <div>
+      {course.parts.map(part => 
+        <p key = {part.id}>
+          {part.name} {part.exercises}
+        </p>
+      )}
+    </div>
+  )
+}
+
+  return(
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+  return(
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
